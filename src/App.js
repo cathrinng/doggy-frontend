c7a5912b33ff378275ea2542e122cb68b2ab02d6
unknown <amandavu@gmail.com>
2022-02-14 15:53:29 +0100

    added sign up

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import './App.css';
-import React from 'react';
-import { HashRouter, Switch, Route } from 'react-router-dom';
+import "./App.css";
+import React from "react";
+import { HashRouter, Switch, Route } from "react-router-dom";
 import Home from "./components/Home";
+import SignUp from "./components/SignUp";
 
 function App() {
   return (
     <HashRouter>
       <h1>Who let the dogs out?</h1>
-    <Switch>
-      <Route path='/' exact component={Home}></Route>
-    </Switch>
+      <Switch>
+        <Route path="/" exact component={Home}></Route>
+        <Route path="/signup" exact component={SignUp}></Route>
+      </Switch>
     </HashRouter>
   );
 }
