dd92d4d6481a6a553d284d9d2eb29169b28f90d3
anders <anders.indregard@gmail.com>
2022-02-16 11:48:01 +0100

    app.js

--- src/App.js
+++ src/App.js
@@ -3,15 +3,75 @@ import React from 'react';
 import { HashRouter, Switch, Route } from 'react-router-dom';
 import Home from "./components/Home";
 
-function App() {
-  return (
-    <HashRouter>
-      <h1>Who let the dogs out?</h1>
-    <Switch>
-      <Route path='/' exact component={Home}></Route>
-    </Switch>
-    </HashRouter>
-  );
+import Homepage from "./components/Homepage";
+import About from "./components/About";
+import Navbar from "./components/Navbar";
+import Login from "./components/Login";
+import SignUp from "./components/SignUp";
+import Swipecard from "./components/Swipecard";
+import Logout from "./components/Logout";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loggedIn: false,
+    };
+  }
+
+  componentDidMount() {
+    this.handleLoginStatusChange();
+  }
+
+  handleLoginStatusChange() {
+    this.setState({
+      loggedIn: !!localStorage.getItem("doggytoken"),
+    });
+  }
+
+  render() {
+    return (
+      <HashRouter>
+        <Navbar
+          loggedIn={this.state.loggedIn}
+          onLoginChange={this.handleLoginStatusChange.bind(this)}
+        />
+        <Switch>
+          <Route path="/" exact component={Homepage}></Route>
+          <Route path="/about" component={About}></Route>
+          <Route
+            path="/login"
+            render={(routeProps) => (
+              <Login
+                {...routeProps}
+                onLoginChange={this.handleLoginStatusChange.bind(this)}
+              />
+            )}
+          />
+          <Route
+            path="/logout"
+            render={(routeProps) => (
+              <Logout
+                {...routeProps}
+                onLoginChange={this.handleLoginStatusChange.bind(this)}
+              />
+            )}
+          />
+          <Route
+            path="/signup"
+            render={(routeProps) => (
+              <SignUp
+                {...routeProps}
+                onLoginChange={this.handleLoginStatusChange.bind(this)}
+              />
+            )}
+          />
+          <Route path="/swipecard" component={Swipecard}></Route>
+        </Switch>
+      </HashRouter>
+    );
+  }
 }
 
 export default App;
