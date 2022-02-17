e21b730df1f295a84823e34d510f25bab3c548e0
unknown <cathrin.nguyen@live.no>
2022-02-17 14:32:59 +0100

    added feed

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,5 +1,5 @@
 import React, { Component, Fragment } from "react";
-import { getUserMatchesById, getMessages } from "../services/dogs";
+import { getUserMatchesById, getMessages, getUsersById } from "../services/dogs";
 
 class Feed extends React.Component {
   constructor(props) {
@@ -7,19 +7,46 @@ class Feed extends React.Component {
 
     this.state = {
       matches: [],
-      messages: [],
+      matchArray: []
+      // messages: [],
+      // users: [],
     }
   }
 
   async loadFeed() {
-    const messages = await getMessages();
-    console.log(messages);
-    const matches = await getUserMatchesById(myId, matchId);
-    console.log(matches);
+    // const messages = await getMessages();
+    const matches = await getUserMatchesById(4);
+    const users = await getUsersById();
+
+    const getMatchId = matches.map((matchId) => {
+     return matchId.user_who_matched;
+    })
 
     this.setState({
       matches,
-      messages,
+      matchArray: getMatchId
+      // messages,
+      // users,
     })
+    
+    console.log("matches:", matches);
+    console.log("matchId:", getMatchId);
+
+  }
+
+  componentDidMount() {
+    this.loadFeed();
+  }
+
+  render() {
+    const matchElements = getUsersById(this.state.matchArray);
+    console.log("matchlist:", matchElements);
+
+    return (
+      <div>
+      </div>
+    )
   }
 }
+
+export default Feed;
