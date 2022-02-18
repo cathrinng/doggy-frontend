4f2e4963320ea924529f371ab48013130ba1b040
unknown <cathrin.nguyen@live.no>
2022-02-18 16:05:16 +0100

    fixed getmessagebyuserid

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,6 +1,8 @@
 import React from "react";
-import { getUserMatchesById, getMessages} from "../services/dogs";
+import { getUserMatchesById, getMessagesByUserId } from "../services/dogs";
+import { formatDistance } from "date-fns";
 import jwtDecode from "jwt-decode";
+import { Link } from 'react-router-dom';
 
 
 class Feed extends React.Component {
@@ -8,51 +10,104 @@ class Feed extends React.Component {
     super(props);
 
     this.state = {
-      matches: [],  
-     
+      matches: [],
+      newMessages: [],
+      payload: {},
+      isLoading: true,
+      error: null,
     }
   }
 
   async loadFeed() {
-    const matches = await getUserMatchesById(4);
+    this.setState({
+      ...this.state,
+      isLoading: true,
+    })
+
+    const token = localStorage.getItem("doggytoken");
+    const payload = jwtDecode(token);
+    const matches = await getUserMatchesById(payload.id);
+    const newMessages = await getMessagesByUserId();
+    console.log(newMessages);
+
     this.setState({
       matches,
+      newMessages,
+      payload,
+      isLoading: false,
     })
   }
 
-  componentDidMount() {
-    const token = localStorage.getItem("doggytoken");
-    const payload = jwtDecode(token);
-
-    if (token) {
-      this.setState({
-        payload: payload,
-      });
-     
-      console.log("you are logged inn as " + payload.surname);
+  async componentDidMount() {
+    try {
+     await this.loadFeed(); 
     }
-  
-    this.loadFeed();
-    
+    catch (error) {
+      this.setState({ error })
+    }
   }
 
   render() {
-    const renderMatches = this.state.matches.map((matchInfo)=>{
-      return(
-        <div key={matchInfo.id} className="match_info">
-          <img src ={matchInfo.img_url} alt="" />
-          
-         {matchInfo.surname}
-         {matchInfo.firstname}
-         {matchInfo.age}
-          
-          
+    const {
+      matches,
+      isLoading,
+      newMessages,
+      error,
+    } = this.state;
+
+    if(error) {
+      return (
+        <div>Ops something went wrong! Unable to load feed: {error.message} </div>
+      )
+    }
+
+    if(isLoading) {
+      return (
+        <div>Loading feed...</div>
+      )
+    }
+
+    const renderMessages = newMessages.map(({ id, from_img_url, from_firstname, from_surname, to_user_id, from_user_id, message, created_at }) => {
+      const timeAgo = formatDistance(
+        new Date(created_at),
+        new Date(),
+        { addSuffix: true }
+      );
+
+
+      return (
+        <div key={id}>
+          <Link to={`/messages/${from_user_id}/${to_user_id}`}>
+          <p><img className="img-message" src={from_img_url}/> {from_firstname} {from_surname} - {timeAgo}</p>
+          <p>{message}</p> 
+          </Link>
         </div>
+      );
+    })
+
+    const renderMatches = matches.map((matchInfo)=>{
+      return(
+        
+          <div key={matchInfo.id} className="match_info">
+            <Link to={`/messages/${matchInfo.user_who_matched}`}>
+              <img src ={matchInfo.img_url} alt=""/>
+              {matchInfo.surname}
+              {matchInfo.firstname}
+              {matchInfo.age}
+            </Link>
+          </div>
       )
     })
+
     return (
-      <div className="match_info_redrer">
-        {renderMatches}
+      <div className="match-container">
+        <h1>Feed for {this.state.payload.firstname}</h1>
+        <div className="match-box">
+          {renderMatches}
+        </div>
+        <div className="message-box">
+          {renderMessages}
+        </div>
       </div>
     )
   }
